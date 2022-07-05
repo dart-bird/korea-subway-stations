185572026e04006a88d0e2893f3f03439b331251
dart-bird(auto) <dartbird.yu@gmail.com>
2022-07-05 20:55:50 +0900

    Change git config params

--- commit_push.py
+++ commit_push.py
@@ -1,7 +1,7 @@
 import subprocess
 
-subprocess.call(['git', 'config', '--local user.email', '"dartbird.yu@gmail.com"'])
-subprocess.call(['git', 'config', '--local user.name', '"dart-bird(auto)"'])
+subprocess.call(['git', 'config', '--local', 'user.email', 'dartbird.yu@gmail.com'])
+subprocess.call(['git', 'config', '--local', 'user.name', 'dart-bird(auto)'])
 subprocess.call(['git', 'remote', 'remove', 'origin'])
 subprocess.call(['git', 'remote', 'add', 'origin', 'https://${{secrets.GIT_TOKEN}}@github.com/dart-bird/korea-subway-stations.git'])
 subprocess.call(['git', 'pull', 'origin', 'main'])
